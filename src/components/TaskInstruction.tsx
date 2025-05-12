dede771d560d8a91faa3e8933fea4841e7fb3564
hiro2620 <ihiro2620@gmail.com>
2025-05-12 15:19:33 +0900

    文字サイズ調整

--- src/components/TaskInstruction.tsx
+++ src/components/TaskInstruction.tsx
@@ -12,7 +12,7 @@ export default function TaskInstruction({ task }: TaskInstructionProps) {
   return (
     <div className="flex flex-col items-center justify-center w-full h-screen bg-black text-white">
       {/* <div className="text-2xl font-bold mb-4">タスク #{task.id}</div> */}
-      <div className="text-4xl max-w-2xl text-center">{task.description}</div>
+      <div className="text-5xl max-w-2xl text-center">{task.description}</div>
     </div>
   );
 }
